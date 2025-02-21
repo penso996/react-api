8d9f51790a47b653e6fcd1128a89cd427815cd58
penso996 <penso.andrea@outlook.it>
2025-02-21 13:51:12 +0100

    working on form to send data to be

--- src/page_components/Main.jsx
+++ src/page_components/Main.jsx
@@ -2,10 +2,20 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+// initial empty form
+const initialPostData = {
+    title: "",
+    content: "",
+    image: "",
+    tags: [],
+};
+
 export default function Main() {
 
     // useState to handle blogPosts
     const [posts, setPosts] = useState([]);
+    // useState to handle form
+    const [newPost, setNewPost] = useState(initialPostData);
 
     // FUNCTION to handle API request
     function fetchBlogPost() {
@@ -17,9 +27,74 @@ export default function Main() {
     // fetch data once
     useEffect(fetchBlogPost, []);
 
+    // FUNCTION to handle form data
+    function handleFormData(e) {
+        // handle various inputs type
+        const value = e.target.name === "tags" ? e.target.value.split(",") : e.target.value;
+        // use setFormData with form input
+        setNewPost((currentFormData) => (
+            {
+                ...currentFormData,
+                [e.target.name]: value,
+            }
+        ));
+    }
+
+    // FUNCTION to handle form submission
+    function handleSubmit(e) {
+        e.preventDefault(); // prevent empty form
+        axios.post("http://localhost:3000/posts", newPost)
+            .then((res) => {
+                setPosts((prevPosts) => [...prevPosts, res.data]);
+                setNewPost(initialPostData);
+            })
+            .catch(err => {
+                console.error("Error adding post", err);
+            });
+    }
+
     // RETURN
     return (
         <main>
+
+            {/* post form */}
+            <div>
+                <h3>Inserisci un nuovo articolo</h3>
+
+                <form onSubmit={handleSubmit}>
+                    {/* form for blog post title */}
+                    <input type="text"
+                        name="title"
+                        onChange={handleFormData}
+                        value={newPost.title}
+                        placeholder="Title"
+                    />
+                    {/* form for blog post content */}
+                    <input type="text"
+                        name="content"
+                        onChange={handleFormData}
+                        value={newPost.content}
+                        placeholder="Content"
+                    />
+                    {/* form for blog post image */}
+                    <input type="text"
+                        name="image"
+                        onChange={handleFormData}
+                        value={newPost.image}
+                        placeholder="Image"
+                    />
+                    {/* form for blog post category */}
+                    <input type="text"
+                        name="tags"
+                        onChange={handleFormData}
+                        value={newPost.tags.join(", ")}
+                        placeholder="Tags (divisi da virgola)"
+                    />
+
+                    <button>Send</button>
+                </form>
+
+            </div>
 
             {/* post container */}
             {posts.length === 0 ? (<div><h3>No posts to show</h3></div>) : (
